ef3738baf5d0949491accdce8be68312701142ec
Karch05 <149317429+Karch05@users.noreply.github.com>
2024-07-08 11:18:47 +0200

    Add files via upload

--- src/tfg_pkg/include/tfg_pkg/actuator.hpp
+++ src/tfg_pkg/include/tfg_pkg/actuator.hpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <string>
 
 class Actuator : public rclcpp::Node
@@ -8,14 +9,16 @@ class Actuator : public rclcpp::Node
 public:
         Actuator();
 
-        void execute_command(const geometry_msgs::msg::Twist::SharedPtr msg) const;
+        void execute_command(const nav_msgs::msg::Odometry::SharedPtr msg) ;
 
         ~Actuator();
 
 private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
 
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
 
     size_t count_;
+
+    geometry_msgs::msg::Twist next_move_;
 };
